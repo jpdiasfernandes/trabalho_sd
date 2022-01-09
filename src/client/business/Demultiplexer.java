4a5a0f8d6c8dcb1bcf9f65f7026d6d721d984a6a
Parola <parolajc@hotmail.com>
2022-01-09 13:06:11 +0000

    fix await and signal withou lock's

--- src/client/business/Demultiplexer.java
+++ src/client/business/Demultiplexer.java
@@ -35,9 +35,16 @@ public class Demultiplexer implements Runnable{
             // read the Frame from Socket
             var reply = tagged.receive();
             // transparência
-            if (reply instanceof Reply){
-                replies.put(reply.getTag(),reply);
-                conditions.get(reply.getTag()).signal();
+            if (reply != null){
+                System.out.println("Chegou uma resposta e ela é diferente de null!");
+                l.lock();
+                try {
+                    replies.put(reply.getTag(), reply);
+                    System.out.println("Vou acordar a TAG: " + reply.getTag());
+                    conditions.get(reply.getTag()).signal();
+                }finally {
+                    l.unlock();
+                }
             }else{
                 // TODO
             }
@@ -45,26 +52,34 @@ public class Demultiplexer implements Runnable{
     }
 
     public Reply service(Request request){
+        Reply reply = null;
         Short tagSend;
         try{
             l.lock();
             tagSend = currentTag;
             Condition c = this.l.newCondition();
             conditions.put(tagSend,c);
+            System.out.println("Enviei o request e vou adormecer até chegar o reply");
             tagged.send(tagSend,request);
             currentTag++;
+
+            while (reply == null){
+                try {
+                    conditions.get(tagSend).await();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Me acordaram! Reply chegou, vou pegar a reply com tag: " + tagSend);
+                reply = replies.get(tagSend);
+
+                if (reply != null) {
+                    System.out.println("... E a reply é diferente de null!");
+                }else{
+                    System.out.println("Fogo, afinal me acordaram e deram-me algo nulo :(");
+                }
+            }
         }finally {
             l.unlock();
-        }
-
-        Reply reply = null;
-        while (reply == null){
-            try {
-                conditions.get(tagSend).await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            reply = replies.get(currentTag);
         }
 
         return reply;
