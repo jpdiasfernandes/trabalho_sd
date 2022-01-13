2ddb844ff0804aad47e24ef180472bc820262033
vasco1m <vasco.matos.2001@gmail.com>
2022-01-13 22:23:06 +0000

    deserializer

--- src/server/frames/SerializerFrame.java
+++ src/server/frames/SerializerFrame.java
@@ -1,8 +1,17 @@
 package server.frames;
 
 public class SerializerFrame {
-    public int opCode;
-    public byte[] data;
     public short tag;
+    public int opCode;
+    public int size;
+    public byte[] data;
+
     public String token;
+
+    public SerializerFrame(short tag, int opCode, int size, byte[] data) {
+        this.tag = tag;
+        this.opCode = opCode;
+        this.size = size;
+        this.data = data;
+    }
 }
