c54670a00cf2b65d569c337502cb4051bfc4bab3
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2022-01-13 10:11:56 +0000

    implement basic functionalities of Middleware
    - Implementei submit, putResponse, retrieve e bufferConsume
    
    - Alterei o BoundedBuffer para ser um par de id_sessao e Frame para
      saber onde colocar no map de resposta

--- src/server/stub/Middleware.java
+++ src/server/stub/Middleware.java
@@ -1,14 +1,28 @@
 package server.stub;
 
+import server.frames.ReplySerializerFrame;
+import server.frames.SerializerFrame;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Middleware {
     public BoundedBuffer buffer;
-    public BoundedMap map;
+    public Map<Integer, ReplySerializerFrame> responseMap;
     public Map<String,String> tokens; //token,username
-    private Lock lm = new ReentrantLock();
+    private Lock tokenL = new ReentrantLock();
+    private Lock responseL = new ReentrantLock();
+    private Condition responseCond = responseL.newCondition();
+
+    public Middleware() {
+        this.buffer = new BoundedBuffer();
+        this.responseMap = new HashMap<>();
+        this.tokens = new HashMap<>();
+    }
 
     //TODO adicionar controlo de concorrência
     public String putToken(String username, String pwd){
@@ -19,9 +33,49 @@ public class Middleware {
 
     public String getUserName(String token){
         String userName = null;
-        lm.lock();
+        tokenL.lock();
         userName = tokens.get(token);
-        lm.unlock();
+        tokenL.unlock();
         return userName;
+    }
+
+    public void submit(SerializerFrame f, int sessionID) {
+        try {
+            buffer.put(f, sessionID);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void putResponse(ReplySerializerFrame reply, int sessionID) {
+        responseL.lock();
+        responseMap.put(sessionID, reply);
+        responseCond.signal();
+        responseL.unlock();
+    }
+    public ReplySerializerFrame retrieve(int sessionID) {
+        responseL.lock();
+        try {
+            ReplySerializerFrame reply = responseMap.get(sessionID);
+            while(reply == null) {
+                responseCond.await();
+                reply = responseMap.get(sessionID);
+            }
+            return reply;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            responseL.unlock();
+        }
+    }
+
+    public Map.Entry<Integer, SerializerFrame> bufferConsume() {
+        try {
+            return buffer.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
